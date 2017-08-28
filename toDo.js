30d8613aad026af3f661feadb39a9e7a7a3917ed
Rawan Abou Dehn <rawan.ad7@gmail.com>
2017-08-28 19:48:02 +0200

    Delete to-do
    Delete pre-written to-dos and use jQuery to display a trash can on hover over a to-do and it can be clicked to delete it

--- toDo.js
+++ toDo.js
@@ -2,15 +2,30 @@ $('input').keypress(function(event){
 	// When enter is pressed
 	if(event.which === 13){
 		// add the todo to the ul
-		$("ul").append('<li class="item">' + $(this).val() + '</li>');
+		$("ul").append('<li class="item"><span class="delete"> X </span> ' + $(this).val() + '</li>');
 		// empty the input
-		$(this).val("")
+		$(this).val("");
 	}	
 });
 
 // When the plus sign is clicked
 $('#toggleInput').click(function(){
-	// Hide the input
-	$('input').toggleClass('hide');
-})
+	// Toggle the input
+	$('input').toggle();
+});
 
+$(document)
+	// when hover over  li
+	.on("mouseenter", "li", function() {
+		// Display the span (trash can)
+		$(this).children().show();
+	})
+	// when leaveing li
+	.on("mouseleave", "li", function() {
+		// Hide the span (trash can)
+		$(this).children().hide();
+	})
+	// Delete a todo
+	.on("click", ".delete", function(){
+		$(this).parent().remove();
+	})
